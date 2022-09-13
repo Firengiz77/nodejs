bf30c83ed39c7af944c16992360238493f43d1c4
Firengiz77 <firengizsariyeva77@gmail.com>
2022-09-13 21:11:40 +0400

    ders2

---
+++ function.js
@@ -0,0 +1,31 @@
+// // ders 2
+// // function makefile(){
+// //     console.log('dosya olustur');
+// // }
+// // makefile();
+
+// function makefile(callback){
+//    //console.log('dosya olustur');
+//     let file = {
+//         name: "Js file"
+//     }
+//     callback(file)
+// }
+// makefile(function(file){
+//   //  console.log('dosya olusturulmaya basladi')
+//     console.log(`${file.name}`);
+// })
+
+
+
+// let sum = function (x,y){
+//     return x+y
+// }
+
+// let sum = (x,y) => x+y
+// let hasil = (x,y,z) => x*y*z
+// console.log(sum(5,6));
+// console.log(hasil(1,2,3))
+
+let sayname = (name)=> `my name is ${name}`
+console.log(sayname('Fira'))
